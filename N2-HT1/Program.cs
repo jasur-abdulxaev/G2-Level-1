035fee7095fd3a16a1474f44d530be22f836e752
jasur-abdulxaev <abdulkhaevjasur@gmail.com>
2026-05-06 22:08:20 +0500

    N2 added

---
+++ N2-HT1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+string name;
+string secondName;
+byte age;
+
+Console.Write("Ismingizni kiriting: ");
+name = Console.ReadLine();
+
+Console.Write("Otangizni ismi: ");
+secondName = Console.ReadLine();
+
+Console.Write("Yoshingizni kiriting: ");
+age = byte.Parse(Console.ReadLine());
+
+Console.WriteLine($"Ismingiz: {name}, Otangizni ismi: {secondName}, Yoshingiz: {age}");
+
+
