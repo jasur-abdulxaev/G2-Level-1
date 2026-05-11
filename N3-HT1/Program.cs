d2ae8d633cf604334b3c0903d497a022caafa097
jasur-abdulxaev <abdulkhaevjasur@gmail.com>
2026-05-11 16:19:14 +0500

    Masala 8 added

---
+++ N3-HT1/Program.cs
@@ -0,0 +1,24 @@
+﻿// 8 - task;
+Console.Write("Oy raqamini kiriting: ");
+int month = int.Parse(Console.ReadLine());
+
+if (month == 12 || month == 1 || month == 2)
+{
+    Console.WriteLine("Qish");
+}
+else if (month == 3 || month == 4 || month == 5)
+{
+    Console.WriteLine("Bahor");
+}
+else if (month == 6 || month == 7 || month == 8)
+{
+    Console.WriteLine("Yoz");
+}
+else if (month == 9 || month == 10 || month == 11)
+{
+    Console.WriteLine("Kuz");
+}
+else
+{
+    Console.WriteLine("Noto'g'ri oy raqami kiritildi.");
+}
